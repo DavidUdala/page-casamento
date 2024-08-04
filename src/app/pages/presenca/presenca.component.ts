940e0dd6e838e1a68c79cccaa13368b8cc40818c
DavidUdala <david_udala@hotmail.com>
2024-08-04 16:49:49 -0300

    texto para whatsapp finalizado

--- src/app/pages/presenca/presenca.component.ts
+++ src/app/pages/presenca/presenca.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./presenca.component.css']
 })
 export class PresencaComponent implements OnInit {
-  confirmationForm!: FormGroup ;
+  confirmationForm!: FormGroup;
   isSubmitted = false;
 
   constructor(private fb: FormBuilder) {
@@ -16,8 +16,7 @@ export class PresencaComponent implements OnInit {
   ngOnInit(): void {
     this.confirmationForm = this.fb.group({
       name: ['', Validators.required],
-      attendance: ['', Validators.required],
-      comparecera: ['true', Validators.required],
+      comparecera: ['true'],
       quantidadeAdultos: ['', Validators.required],
       quantidadeCriancas: ['', Validators.required],
       termsAccepted1: [false, Validators.requiredTrue],
@@ -27,16 +26,34 @@ export class PresencaComponent implements OnInit {
 
   onSubmit() {
     this.isSubmitted = true;
+    console.log('valido? ', this.confirmationForm.valid);
     if (this.confirmationForm.valid) {
       this.openWhatsApp();
     }
   }
 
   openWhatsApp() {
-    const phoneNumber = '5511954394472'; // Substitua pelo número de telefone com o código do país e DDD
-    const message = `Olá, me chamo ${this.confirmationForm.get('name')?.value}, gostaria de confirmar minha presença no evento!`; // Mensagem predefinida
+    const phoneNumber = '5511954394472';
+    const message = this.textoConfirmacaoPresenca(); // Mensagem predefinida
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   }
 
+  textoConfirmacaoPresenca(): string {
+    let msg = `Olá, me chamo ${this.confirmationForm.get('name')?.value}`;
+
+    console.log(this.confirmationForm.get('comparecera')?.value);
+
+    if (this.confirmationForm.get('comparecera')?.value != 'true') {
+      msg += ` ,infelizmente não poderei comparecer ao casamento.`
+      return msg;
+    }
+
+    msg += ` ,gostaria de confirmar minha presença`
+    msg += ` e de mais ${this.confirmationForm.get('quantidadeAdultos')?.value - 1} adulto(s)`
+    msg += ` e ${this.confirmationForm.get('quantidadeCriancas')?.value} criança(s).`
+
+
+    return msg;
+  }
 }
